02c9cea0fc2f979e1b8e6e7c9a471b39fee6cfe6
root <admin@example.com>
2021-09-09 09:42:16 +0800

    C_1.6

---
+++ ConfigDemo.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+class ConfigDemo():
+    """这个类是用于配置Flask的全局变量"""
+    def __init__(self, p_app):
+        self.app = p_app
+        self.set_config()
+
+    def set_config(self):
+        """
+        Function:设施配置变量, 这里的配置变量是在程序开发的时候确定了的
+        :return:
+        """
+        self.app.config['DEMO_NAME'] = "FlaskDemo"
+
+
+
--- app.py
+++ app.py
@@ -1,10 +1,26 @@
 
-
 from flask import Flask
 
-app = Flask(__name__)
+from ConfigDemo import ConfigDemo
+
+app = Flask(__name__)  # 创建FlaskApp
+app = ConfigDemo(app).app  # 读入全局配置变量
 
 @app.route("/")
 def index():
     return "<H1>Hello, World!</H1>"
 
+@app.route("/hi")
+@app.route("/hello")
+def say_hello():
+    return "<H1>Say Hello!</H1>"
+
+@app.route("/greet/", defaults={"name": "Programmer"})
+@app.route("/greet/<name>")
+def greet(name):
+    return "<H1>Welcome, %s!</H1>" % name
+
+@app.route("/demoname")
+def demoname():
+    v_demo_name = app.config['DEMO_NAME']
+    return "<H1>I am {0}!</H1>".format(v_demo_name)
