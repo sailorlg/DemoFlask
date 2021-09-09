c5862f9e9b6a5ab95d19faeede75729edbd4396b
root <admin@example.com>
2021-09-09 13:43:15 +0800

    C_2.2.2

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 
-from flask import Flask
+from flask import Flask, url_for, request
+import click
 
 from ConfigDemo import ConfigDemo
 
@@ -13,7 +14,18 @@ def index():
 @app.route("/hi")
 @app.route("/hello")
 def say_hello():
-    return "<H1>Say Hello!</H1>"
+    """
+    Function:获取从浏览器传过来的参数name的值, 并显示
+    :return:
+    """
+    print(request)
+    print(request.args)
+
+    v_name = request.args.get('name')
+    if v_name is None:  # 如果浏览器没有数据name的值
+        v_name = "Nobody"
+
+    return "<H1>Say Hello to {}!</H1>".format(v_name)
 
 @app.route("/greet/", defaults={"name": "Programmer"})
 @app.route("/greet/<name>")
@@ -24,3 +36,23 @@ def greet(name):
 def demoname():
     v_demo_name = app.config['DEMO_NAME']
     return "<H1>I am {0}!</H1>".format(v_demo_name)
+
+@app.route("/geturl")
+def geturl():
+    """
+    Function:测试url_for()函数
+    :return:
+    """
+    v_url = url_for("greet")
+    v_url_external = url_for("greet", _external=True)
+    return ("Greet's internal URL is {0}, external URL is {1}.".format(v_url, v_url_external))
+
+@app.cli.command()
+def get_demo_name():
+    """
+    Function: 测试Flask CLI命令的功能
+    在进入虚拟环境的命令行中输入"flask get-demo-name"可执行该命令
+    :return:
+    """
+    v_demo_name = app.config['DEMO_NAME']
+    click.echo(v_demo_name)
